88af7dc15b93917c575adc999fe9dbdb57b53f11
xiaohaohua1 <pass@qq.com>
2023-04-22 11:26:13 +0800

    fix commit1

--- src/main/java/test1/Test1.java
+++ src/main/java/test1/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("idea 集成 github");
         System.out.println("fix fix ");
+        System.out.println("fix2 fix2");
     }
 }
