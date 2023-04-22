c5791be0d5e9f508f9f9483bfdc56100ad3547a5
rainbow13145206 <128973589+rainbow13145206@users.noreply.github.com>
2023-04-22 11:30:11 +0800

    Update Test1.java

--- src/main/java/test1/Test1.java
+++ src/main/java/test1/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
         System.out.println("idea 集成 github");
         System.out.println("master master master");
         System.out.println("master master master");
+        System.out.println("come here to push me");
     }
 }
